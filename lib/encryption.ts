de2d72ee240cf763b03a040ec9bc3165a0c78051
Vishal-Marvel <vishal.pvn.edu@gmail.com>
2024-01-29 22:44:52 +0530

    fix: IV(salt) length set to 16

--- lib/encryption.ts
+++ lib/encryption.ts
@@ -7,7 +7,7 @@ export function encryptReq(password: string, data: string) {
     try {
         const key = crypto.pbkdf2Sync(password, salt, 20000, 32, 'sha512');
 
-        const cipher = crypto.createCipheriv("aes-256-cbc", key, salt);
+        const cipher = crypto.createCipheriv("aes-256-cbc", key, salt.substring(0,16));
         let encryptedText = cipher.update(data, "utf-8", "hex");
         encryptedText += cipher.final("hex");
         return encryptedText;
@@ -19,7 +19,7 @@ export function encryptReq(password: string, data: string) {
 export function decryptReq(password: string, encrptedString: string) {
     const key = crypto.pbkdf2Sync(password, salt, 20000, 32, 'sha512');
 
-    const decipher = crypto.createDecipheriv("aes-256-cbc", key, salt);
+    const decipher = crypto.createDecipheriv("aes-256-cbc", key, salt.substring(0,16));
     let decryptedText = decipher.update(encrptedString, "hex", "utf-8");
     decryptedText += decipher.final("utf-8");
     return decryptedText;
