19ac6bda8077090e3a27f96dbb80417d5600a0e1
Vishal-Marvel <vishal.pvn.edu@gmail.com>
2024-01-29 22:43:39 +0530

    fix: Reduced the iterations on encryption and decryption

--- lib/encryption-server.ts
+++ lib/encryption-server.ts
@@ -17,7 +17,7 @@ export function generateSaltFromPassword(password: string) {
 export function encrypt(text: string, password: string) {
 
     const salt = generateSaltFromPassword(password);
-    const key = crypto.pbkdf2Sync(password, salt, 100000, 32, 'sha512');
+    const key = crypto.pbkdf2Sync(password, salt, 50000, 32, 'sha512');
     const cipher = crypto.createCipheriv("aes-256-cbc", key, salt);
     let encryptedText = cipher.update(text, "utf-8", "hex");
     encryptedText += cipher.final("hex");
@@ -30,7 +30,7 @@ export async function compareStrings(originalString: string, hashedString: strin
 
 export function decrypt(password: string, encrptedString: string, salt: string) {
 
-    const key = crypto.pbkdf2Sync(password, salt, 100000, 32, 'sha512');
+    const key = crypto.pbkdf2Sync(password, salt, 50000, 32, 'sha512');
 
     const decipher = crypto.createDecipheriv("aes-256-cbc", key, salt);
     let decryptedText = decipher.update(encrptedString, "hex", "utf-8");
