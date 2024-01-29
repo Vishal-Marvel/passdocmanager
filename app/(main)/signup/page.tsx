212776fdbd9b0aae0e6523328cd07f7079e7791c
Vishal-Marvel <vishal.pvn.edu@gmail.com>
2024-01-29 22:44:01 +0530

    feat: Password Validation Added

--- app/(main)/signup/page.tsx
+++ app/(main)/signup/page.tsx
@@ -1,6 +1,6 @@
 "use client"
-import {useForm} from 'react-hook-form';
-import {zodResolver} from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
 import {
     Form,
@@ -10,17 +10,18 @@ import {
     FormLabel,
     FormMessage,
 } from '@/components/ui/form';
-import {Input} from '@/components/ui/input';
-import {Button} from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
 
-import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import axios from "axios";
-import {useRouter} from "next/navigation";
-import {useEffect, useState} from "react";
-import {Progress} from "@/components/ui/progress";
+import { useRouter } from "next/navigation";
+import { useEffect, useRef, useState } from "react";
+import { Progress } from "@/components/ui/progress";
 import { encryptReq } from '@/lib/encryption';
 import { toast } from 'sonner';
-import { Loader2 } from 'lucide-react';
+import { AlertCircle, Check, Loader2, X } from 'lucide-react';
+import { cn } from '@/lib/utils';
 
 
 const formSchema = z.object({
@@ -30,16 +31,20 @@ const formSchema = z.object({
 });
 
 export default function SignUpComponent() {
-    const [validate, setValidate] = useState({
-        hasLow: false,
-        hasCap: false,
-        hasNumber: false,
-        has8digit: false,
-        hasSymbol: false
-    });
+    const [isConfirmSame, setIsConfirmState] = useState(false);
+
+    const [requirementList, setRequirementList] = useState([
+        { regex: /.{8,}/, text: "Minimum of 8 characters", state: false },
+        { regex: /[0-9]/, text: "At least one number", state: false },
+        { regex: /[a-z]/, text: "At least one lowercase letter", state: false },
+        { regex: /[^A-Za-z0-9]/, text: "At least one special character", state: false },
+        { regex: /[A-Z]/, text: "At least one uppercase letter", state: false },
+    ]);
 
 
-    const strength = Object.values(validate).reduce((a, item) => a + (item ? 1 : 0), 0);
+    // const strength = Object.values(validate).reduce((a, item) => a + (item ? 1 : 0), 0);
+    const strength = requirementList.filter(item => item.state).length + (isConfirmSame ? 1 : 0);
+
 
     const feedback = {
         1: "Password is to weak!",
@@ -48,37 +53,6 @@ export default function SignUpComponent() {
         4: "You almost there!",
         5: "Great!! now your password is strong"
     }[strength];
-    const validatePassword = (password: string) => {
-        if (password.match(/\d+/g)) {
-            setValidate((o) => ({...o, hasNumber: true}));
-        } else {
-            setValidate((o) => ({...o, hasNumber: false}));
-        }
-
-        if (password.match(/[A-Z]+/g)) {
-            setValidate((o) => ({...o, hasCap: true}));
-        } else {
-            setValidate((o) => ({...o, hasCap: false}));
-        }
-
-        if (password.match(/[a-z]+/g)) {
-            setValidate((o) => ({...o, hasLow: true}));
-        } else {
-            setValidate((o) => ({...o, hasLow: false}));
-        }
-
-        if (password.length > 7) {
-            setValidate((o) => ({...o, has8digit: true}));
-        } else {
-            setValidate((o) => ({...o, has8digit: false}));
-        }
-        if (password.match(/[^a-zA-Z0-9]+/g)) {
-            setValidate((o) => ({...o, hasSymbol: true}));
-        } else {
-            setValidate((o) => ({...o, hasSymbol: false}));
-        }
-
-    };
 
     const form = useForm({
         resolver: zodResolver(formSchema),
@@ -93,42 +67,68 @@ export default function SignUpComponent() {
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
         try {
 
-            if (values.password !== values.confirmPassword) {
-                form.setError("confirmPassword", {message: "Passwords Need to match"})
-                form.setError("password", {message: "Passwords Need to match"})
-                form.setFocus("password");
-                return;
-            }
-            if (strength < 5) {
-                form.setError("password", {message: "Password is weak"})
-                form.setError("confirmPassword", {message: "Password is weak"})
-                form.setFocus("password");
-                return;
-            }
-            await axios.post("/api/saveUser", {viewPassword:encryptReq(values.password, values.password)});
+            await axios.post("/api/saveUser", { viewPassword: encryptReq(values.password, values.password) });
+            router.push('/');
             form.reset()
             toast("Password Saved")
-            router.push('/');
+            
         } catch (error) {
+            toast(<><AlertCircle />{error.response.data}</>)
             console.error(error)
         }
     };
 
+
+
     useEffect(() => {
-        if (!isLoading) {
-            const timer = setInterval(() => {
-                validatePassword(form.getValues("password"))
-            }, 500)
-            return () => clearInterval(timer)
+        const handleEvent = () => {
+            const value = form.getValues("password");
+
+            // console.log("event")
+            requirementList.forEach((item, index) => {
+                const isValid = item.regex.test(value);
+                if (isValid) {
+                    const newList = [...requirementList];
+                    newList[index].state = true;
+                    setRequirementList(newList);
+                } else {
+                    const newList = [...requirementList];
+                    newList[index].state = false;
+                    setRequirementList(newList);
+
+                }
+            })
         }
-    })
+        const timer = setInterval(handleEvent, 500)
+        return () => clearInterval(timer);
+
+    }, [])
+
+    useEffect(() => {
+        const handleEvent = () => {
+            const passwordValue = form.getValues("password");
+            const confirmValue = form.getValues("confirmPassword")
+
+            // console.log("event")
+            if (passwordValue === confirmValue && passwordValue !== "") {
+                setIsConfirmState(true);
+            } else {
+                setIsConfirmState(false);
+            }
+
+        }
+        const timer = setInterval(handleEvent, 500)
+        return () => clearInterval(timer);
+
+    }, [])
+
 
     return (
         <div className={"w-full h-screen flex justify-center items-center"}>
             <Card className="lg:w-1/3 md:w-2/4 w-4/5">
                 <CardHeader>
                     <CardTitle>Set Password</CardTitle>
-                    <CardDescription>This password is used for accessing the documents and passwords stored. <strong>This
+                    <CardDescription className='text-justify'>This password is used for accessing the documents and passwords stored. <strong>This
                         is different from the login password</strong></CardDescription>
                 </CardHeader>
                 <CardContent>
@@ -138,7 +138,7 @@ export default function SignUpComponent() {
                                 disabled={isLoading}
                                 name={"password"}
                                 control={form.control}
-                                render={({field}) => (
+                                render={({ field }) => (
                                     <FormItem>
                                         <FormLabel>Password:</FormLabel>
                                         <FormControl>
@@ -150,15 +150,15 @@ export default function SignUpComponent() {
                                                 type={"password"}
                                             />
                                         </FormControl>
-                                        <FormMessage/>
+                                        <FormMessage />
                                     </FormItem>
                                 )
-                                }/>
+                                } />
                             <FormField
                                 disabled={isLoading}
                                 name={"confirmPassword"}
                                 control={form.control}
-                                render={({field}) => (
+                                render={({ field }) => (
                                     <FormItem>
                                         <FormLabel>Confirm Password:</FormLabel>
                                         <FormControl>
@@ -169,17 +169,30 @@ export default function SignUpComponent() {
                                                 type={"password"}
                                             />
                                         </FormControl>
-                                        <FormMessage/>
+                                        <FormMessage />
                                     </FormItem>
                                 )
-                                }/>
-                            {strength > 0  &&
-                                <Progress value={strength / 5 * 100}/>
+                                } />
+                            {!isLoading && strength > 0 && strength < 5 &&
+                                <>
+                                    <Progress value={strength / 5 * 100} />
+                                    <span className={"text-sm text-center mt-2"}>{feedback}</span>
+
+
+                                    <div className='grid grid-cols-1 gap-1'>
+                                        {requirementList.map((req, index) => (
+                                            <span key={index} className={cn("text-sm flex m-0 items-center gap-2 transition-all duration-400 ease-in", req.state ? "text-gray-600 line-through" : "text-black")}>{req.state ? <Check className="h-4 w-4 m-0 text-inherit" /> : <X className="h-4 w-4 m-0 text-inherit" />} {req.text}</span>
+                                        ))}
+
+                                    </div>
+                                </>
                             }
-                            <span className={"text-sm text-center mt-2"}>{feedback}</span>
+                            {!isLoading && strength>0 &&
+                                <span className={cn("text-sm flex m-0 items-center gap-2 transition-all duration-400 ease-in", isConfirmSame ? "text-gray-600 line-through" : "text-black")}>{isConfirmSame ? <Check className="h-4 w-4 m-0 text-inherit" /> : <X className="h-4 w-4 m-0 text-inherit" />} Both Password And Confirm Password must be same</span>
+                            }
 
                             <div className="flex flex-col space-y-1.5 pt-2">
-                                <Button disabled={isLoading} type="submit">{isLoading && <Loader2 className='h-4 w-4 animate-spin'/>}Sign Up</Button>
+                                <Button disabled={isLoading || (strength < 6)} type="submit">{isLoading && <Loader2 className='h-4 w-4 animate-spin mr-2' />}Sign Up</Button>
                             </div>
                         </form>
                     </Form>
