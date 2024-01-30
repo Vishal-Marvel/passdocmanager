07df8f7ee9e58f20fe6c1da180b328be9e04507e
Vishal-Marvel <vishal.pvn.edu@gmail.com>
2024-01-30 14:30:53 +0530

    fix: fixed the text-center property for title

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const AuthLayout = ({children} : {children: React.ReactNode}) => {
     return (
         <div className="h-screen flex flex-col items-center justify-center">
-            <div className={"p-2 mt-4 font-bold text-2xl"}>
+            <div className={"p-2 mt-4 font-bold text-2xl text-center"}>
                 <span>Sign in To Continue to PASSWORD and DOCUMENT manager</span>
             </div>
             {children}
