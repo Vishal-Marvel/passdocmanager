07f842bbd4af54c64b4c79173f610aaefba065e5
Vishal-Marvel <vishal.pvn.edu@gmail.com>
2024-01-30 14:36:11 +0530

    feat: made the table sze and table col span for footer responsive

--- components/PasswordsTable.tsx
+++ components/PasswordsTable.tsx
@@ -34,7 +34,7 @@ function isMobileView() {
     const viewportWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 
     // Define the threshold width for mobile view
-    const mobileWidthThreshold = 1000; // Adjust this value as needed for your design
+    const mobileWidthThreshold = 1030; // Adjust this value as needed for your design
 
     // Check if the viewport width is less than the mobile threshold
     return viewportWidth < mobileWidthThreshold;
@@ -46,6 +46,7 @@ export const PasswordsTable = () => {
     const [newField, setNewField] = useState(false);
     const [search, setSearch] = useState("");
     const [isLoading, setIsLoading] = useState(true);
+    const [span, setSpan] = useState(isMobileView() ? 0 :4);
 
     const getPasswords = async () => {
         console.log("loaing");
@@ -57,10 +58,26 @@ export const PasswordsTable = () => {
     }
     const handleSearch = (value: string) => {
         setSearch(value);
-        const filtered = passwords?.filter((password) => password.key.includes(value));
+        const filtered = passwords?.filter((password) => password.key.toLowerCase().includes(value.toLowerCase())|| password.category.name.toLowerCase().includes(value.toLowerCase()));
         setVisible(filtered);
 
     }
+
+    useEffect(()=>{
+        const handleResize = () =>{
+            const  width  = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+            if (width<1030){
+                // console.log(0)
+                setSpan(0);
+            }else{
+                // console.log(4)
+                setSpan(4)
+            }
+
+        }
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    } ,[])
 
     useEffect(() => {
         getPasswords()
@@ -76,7 +93,7 @@ export const PasswordsTable = () => {
     return (
         <div className="md:w-[80vw] w-full overflow-clip flex flex-col items-center">
             <div className="md:w-[60vw] flex md:flex-row flex-col gap-4 justify-center items-center m-2">
-                <Input className="border-2 " placeholder="Search..." value={search} onChange={(e) => handleSearch(e.target.value)} />
+                <Input className="border-2 " placeholder="Search Key or Category..." value={search} onChange={(e) => handleSearch(e.target.value)} />
                 <AddPassword onSubmitChange={() => setNewField(!newField)} />
             </div>
             {isLoading &&
@@ -90,22 +107,16 @@ export const PasswordsTable = () => {
                 </div>
             }
 
-            {/* <div className="w-full flex flex-wrap md:m-4 p-2">
-
-                {passwords && passwords.length > 0 && visible.map((password, index) => (
-                    <PasswordBox password={password} key={index} />
-                ))}
-            </div> */}
             {passwords && passwords.length > 0 &&
             // <div className="flex justify-center w-full">
-                <ScrollArea className={"h-[60vh] md:w-[60vw] w-[90vw] md:m-4  mb-0"}>
+                <ScrollArea className={"h-[60vh] md:w-[60vw] w-[80vw] md:m-4 mb-0 transition-all duration-200 ease-in"}>
 
                     <Table className="table-auto">
                         <TableHeader className={"sticky top-0 bg-secondary"}>
                             <TableRow className={"text-indigo-950"}>
                                 <TableHead className={"hidden lg:table-cell "}>S. No.</TableHead>
                                 <TableHead className={""}>Key</TableHead>
-                                <TableHead className={""}>Value</TableHead>
+                                <TableHead className={"text-center"}>Value</TableHead>
                                 <TableHead className={"hidden lg:table-cell "}>Category</TableHead>
                                 <TableHead className={"w-1/6 hidden lg:table-cell "}>Updated At</TableHead>
                             </TableRow>
@@ -115,7 +126,7 @@ export const PasswordsTable = () => {
                                 <TableRow key={record.id}>
                                     <TableCell className="hidden lg:table-cell ">{index + 1}</TableCell>
                                     <TableCell className="">{record.key}</TableCell>
-                                    <TableCell><PasswordBox password={record} onSubmitChange={() => setNewField(!newField)}/></TableCell>
+                                    <TableCell className="text-center"><PasswordBox password={record} onSubmitChange={() => setNewField(!newField)}/></TableCell>
                             
                                     <TableCell className={"hidden lg:table-cell "}>{record.category.name}</TableCell>
                                     <TableCell className={"hidden lg:table-cell "}>{record.updatedAt.toLocaleString().slice(0, 10)}</TableCell>
@@ -125,8 +136,8 @@ export const PasswordsTable = () => {
                         </TableBody>
                         <TableFooter className={"sticky bottom-0 bg-secondary"}>
                             <TableRow>
-                                <TableCell colSpan={isMobileView() ? 0 : 4}>Total</TableCell>
-                                <TableCell className="md:text-right">{visible.length} Records(s)</TableCell>
+                                <TableCell colSpan={span}>Total</TableCell>
+                                <TableCell className="lg:text-right text-center">{visible.length} Records(s)</TableCell>
                             </TableRow>
                         </TableFooter>
                     </Table>
