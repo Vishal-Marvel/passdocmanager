c794a8b0f1c0b4794f2ef8c2694474a6ca4e1fce
Vishal-Marvel <vishal.pvn.edu@gmail.com>
2024-02-01 13:12:30 +0530

    feat: Added Reset Password

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,42 +1,38 @@
 import CustomSignOutButton from "@/components/CustomSignOutButton";
-import { SignedIn, SignInButton } from "@clerk/nextjs";
 import { FileCheck2, KeyRound } from 'lucide-react';
 import Link from "next/link";
 import { initProfile } from "@/lib/init-profile";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@/components/ui/tooltip"
+
+import { buttonVariants } from "@/components/ui/button";
 
 
 export default async function Home() {
   const profile = await initProfile();
   return (
-    <div className={"w-full h-screen flex flex-col items-center justify-center"}>
-      <div className={"absolute top-2 right-2"}>
-        <SignedIn >
-          <CustomSignOutButton />
-        </SignedIn>
+    <div className={"m-2 p-2 flex flex-col "}>
+      <div className={"flex w-full justify-end gap-4 sticky"}>
+        <Link href={"/setPasswrod"} className={buttonVariants({ variant: "outline" })}> Reset Password </Link>
+        <CustomSignOutButton />
       </div>
-      <div className={"flex flex-col h-1/2 items-center text-center"}>
-        <span className={"text-3xl font-bold font-poppins"}>PASSWORD AND DOCUMENT MANAGER</span>
-        <div className={"w-full justify-around m-3 p-2 flex pt-8 "}>
-   
-          <Link className={"h-[150px] w-[150px] border-8 border-blue-300  transform-all duration-200 hover:-translate-y-4 rounded-2xl m-2 p-2 shadow-xl grid place-items-center"}
-                  href={"/password"}>
-                  <KeyRound className={"h-20 w-20 p-4"} color={"#93c5fd"} />
-                  <span className="font-semibold text-sm ">Password</span>
+      <div className="h-[60vh] flex flex-col justify-center">
+        <div className={"flex flex-col h-1/2 items-center text-center"}>
+          <span className={"text-3xl font-bold font-poppins"}>PASSWORD AND DOCUMENT MANAGER</span>
+          <div className={"w-1/2 justify-around m-3 p-2 flex pt-8 "}>
+
+            <Link className={"h-[150px] w-[150px] border-8 border-blue-300  transform-all duration-200 hover:-translate-y-4 rounded-2xl m-2 p-2 shadow-xl grid place-items-center"}
+              href={"/password"}>
+              <KeyRound className={"h-20 w-20 p-4"} color={"#93c5fd"} />
+              <span className="font-semibold text-sm ">Password</span>
             </Link>
 
-          <Link className={"h-[150px] w-[150px] border-8 border-blue-300  transform-all duration-200 hover:-translate-y-4 rounded-2xl m-2 p-2 shadow-xl grid place-items-center"}
-            href={"/document"}>
-            <FileCheck2 className={"h-20 w-20 p-4"} color={"#93c5fd"} />
-            <span className="font-semibold text-sm">Document</span>
+            <Link className={"h-[150px] w-[150px] border-8 border-blue-300  transform-all duration-200 hover:-translate-y-4 rounded-2xl m-2 p-2 shadow-xl grid place-items-center"}
+              href={"/document"}>
+              <FileCheck2 className={"h-20 w-20 p-4"} color={"#93c5fd"} />
+              <span className="font-semibold text-sm">Document</span>
 
-          </Link>
+            </Link>
 
+          </div>
         </div>
       </div>
     </div>
