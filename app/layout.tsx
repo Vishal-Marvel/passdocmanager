3d35f8a018e772ee7d4e5ff7cf773e0343f1bccf
Vishal-Marvel <vishal.pvn.edu@gmail.com>
2024-02-01 19:46:48 +0530

    chore: added the redirect to signin component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,41 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import type {Metadata} from "next";
+import {Inter} from "next/font/google";
 import "./globals.css";
 import React from "react";
-import {ClerkProvider} from "@clerk/nextjs";
-import { Toaster } from "@/components/ui/sonner"
-const inter = Inter({ subsets: ["latin"], variable: "--font-sans", });
+import {ClerkProvider, RedirectToSignIn, SignedIn, SignedOut, SignInButton} from "@clerk/nextjs";
+import {Toaster} from "@/components/ui/sonner"
+import Link from "next/link";
+import {buttonVariants} from "@/components/ui/button";
+import {cn} from "@/lib/utils";
+
+const inter = Inter({subsets: ["latin"], variable: "--font-sans",});
 
 export const metadata: Metadata = {
-  title: "Password Document Manager",
-  description: "Manager for Password and Document",
+    title: "Password Document Manager",
+    description: "Manager for Password and Document",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-      <ClerkProvider>
-        <html lang="en">
-          <body className={inter.className}>
-           
-            {children}          
-            
+    return (
+        <ClerkProvider>
+            <html lang="en">
+            <body className={inter.className}>
+            <SignedIn>
+                {children}
+            </SignedIn>
+            <SignedOut>
+
+                <RedirectToSignIn/>
+                {children}
+            </SignedOut>
             <Toaster position="top-right"/>
-          </body>
-          
-        </html>
-      </ClerkProvider>
-  );
+            </body>
+
+            </html>
+        </ClerkProvider>
+    );
 }
